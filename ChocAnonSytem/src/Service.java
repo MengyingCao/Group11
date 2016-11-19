75ceeda768dc1a2c3d0e444c5fa1b9885d6af507
jorden3 <jorden3@pdx.edu>
2016-11-18 19:16:51 -0800

    Done by Neil really.

---
+++ ChocAnonSytem/src/Service.java
@@ -0,0 +1,45 @@
+
+public class Service {
+
+	private int serviceCode;
+	private float fee;
+	private String serviceName;
+	
+	public Service()
+	{
+		serviceCode = -999;
+		fee = -9.99f;
+		serviceName = "Default";
+	}
+	public Service(int passedServiceCode, float passedFee, String passedServiceName)
+	{
+		this.serviceCode = passedServiceCode;
+		this.fee = passedFee;
+		this.serviceName = passedServiceName;
+	}
+	public int getServiceCode() {
+		return serviceCode;
+	}
+
+	public void setServiceCode(int serviceCode) {
+		this.serviceCode = serviceCode;
+	}
+
+	public float getFee() {
+		return fee;
+	}
+
+	public void setFee(float fee) {
+		this.fee = fee;
+	}
+
+	public String getServiceName() {
+		return serviceName;
+	}
+
+	public void setServiceName(String serviceName) {
+		this.serviceName = serviceName;
+	}
+
+
+}
