16c9667a68086007234f633c141eb5b0943a02bf
jorden3 <jorden3@pdx.edu>
2016-11-18 19:59:01 -0800

    Added search by service number

--- ChocAnonSytem/src/ProviderDirectory.java
+++ ChocAnonSytem/src/ProviderDirectory.java
@@ -15,7 +15,7 @@ public class ProviderDirectory
 		directory.put(temp.getName(), temp);	
 	}
 	
-	public void display()
+	public void displayAll()
 	{
 		for(Entry<String, Person> entry : directory.entrySet())
 		{
@@ -36,21 +36,40 @@ public class ProviderDirectory
 		return 0;
 	}
 	
+	public int searchByServiceNum(int found)
+	{
+		for(Entry<String, Person> entry : directory.entrySet())
+		{
+			if(entry.getValue().getService() == found)
+			{
+				entry.getValue().displayServ();
+				return 1;
+			}
+		}
+		return 0;
+	}
+	
 	public static void main(String[] args)
 	{
 		ProviderDirectory map = new ProviderDirectory();
 		Service temp1 = new Service(1234, 300, "PHY");
 		Person	temp = new Person("Jorden", 56, temp1);
-		
 		map.addPro(temp);
+		
+		temp1 = new Service(1, 300, "HY");
 		temp = new Person("Tay", 123, temp1);
 		map.addPro(temp);
+		
+		temp1 = new Service(2, 300, "P");
 		temp = new Person("Butt", 321, temp1);
 		map.addPro(temp);
 
 
 		//System.out.println(directory.size());
 		//map.display();
-		map.searchByProNum(123);
+		//map.searchByProNum(123);
+		map.searchByServiceNum(1234);
+		map.searchByServiceNum(1);
+		map.searchByServiceNum(2);
 	}
 }
