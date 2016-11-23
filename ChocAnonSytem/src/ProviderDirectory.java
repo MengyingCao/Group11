cd2c5f57a8c23ab0bf06dbec4419b5f23af3223a
jorden3 <jorden3@pdx.edu>
2016-11-22 21:20:52 -0800

    New Provider Directory

---
+++ ChocAnonSytem/src/ProviderDirectory.java
@@ -0,0 +1,14 @@
+import java.util.TreeMap;
+import java.util.Map.Entry;
+
+public class ProviderDirectory 
+{
+	private TreeMap<Integer, Service> directory;
+	
+	public ProviderDirectory()
+	{
+		directory = new TreeMap<Integer, Service>();
+	}
+	
+
+}
